46ff7663229a8c56583a558b24229c3ba76bd258
Margo Bozova <margo.bozova@gmail.com>
2017-03-04 12:12:35 +0200

    Create class Office

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function Office() {
-	
+
+// Create class Office
+function Office(id, roomNum) {
+	this.id = id;
+	this.roomNum = roomNum;
+	this.roomSpace = roomSpace;
 }
+
+var exem1 = new Office("#4", "5");
+var exem2 = new Office("#4", "5");

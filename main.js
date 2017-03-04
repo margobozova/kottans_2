2fe694a8a0dfe02bba04532aec63647ef80cae15
Margo Bozova <margo.bozova@gmail.com>
2017-03-04 13:01:44 +0200

    Create polimorphism

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var exem2 = new Office("#4", "5");
 var myOffice = new Office("#4", 6);
 myOffice.turnOnLight();
 
-//наследование
+//Наследование
 //метод call чтоб использовать this из Office
 function AdvancedOffice (id, roomNum, numOfWindows) {
 	Office.call(this, id, roomNum);
@@ -24,3 +24,20 @@ function AdvancedOffice (id, roomNum, numOfWindows) {
 }
 // передаем прототипы с Object в AdvancedOffice
 AdvancedOffice.prototype = Object.create(Office.prototype);
+AdvancedOffice.prototype.constructor = AdvancedOffice;
+
+
+AdvancedOffice.prototype.turnOffLight = function () {
+	console.log("Light is OFF");
+}
+
+AdvancedOffice.prototype.turnOnAndOffLight = function () {
+	console.log("Light is OFF");
+	this.turnOnLight();
+}
+
+//полиморфизм 
+AdvancedOffice.prototype.turnOnLigth = function () {
+	Office.prototype.turnOnLight.call(this);
+	console.log("URAH, LIGTH IS ON");
+}
